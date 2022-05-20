00c637edecd0ecfaf6046327d3fff6d04243ccae
Srinjoy Sanyal <srinjoy.sanyal.001@student.uni.lu>
2022-05-20 18:07:38 +0200

    commit 1

--- src/views/QuillGitHelper.js
+++ src/views/QuillGitHelper.js
@@ -4,18 +4,133 @@ const Simplegit = require('simple-git');
 
 const git = new Simplegit();
 
-git.init().addRemote("origin", "https://github.com/SrinjoySanyal/subrepo.git");
+git.init();
 
-function commonIndices(version0, version1) {
-  let index1 = 0;
+function commonIndicesVersion1(version0, version1) {
+  let index1 = -1;
   let array1 = [];
-  for(element in version0){
-    if(element in version1.slice(index1 + 1, version1.length - 1)){
-      array1.push(version1.indexOf(element));
-      index1 = version0.indexOf(element);
+  let interestElement;
+  for(let element0 = 0; element0 < version0.length; element0++){
+    for(let element1 = index1 + 1; element1 < version1.length; element1++){
+      //console.log(String(element0) + "," + String(element1));
+      if(JSON.stringify(version0[element0]) == JSON.stringify(version1[element1])){
+        
+        interestElement = element1;
+        array1.push(element1);
+        //console.log(JSON.stringify(interestElement));
+        
+      }
     }
+    index1 = interestElement;
   }
   return array1;
+}
+
+function commonIndicesVersion0(version0, version1) {
+  let index0 = -1;
+  let array0 = [];
+  let interestElement;
+  for(let element0 = 0; element0 < version0.length; element0++){
+    for(let element1 = index0 + 1; element1 < version1.length; element1++){
+      //console.log(String(element0) + "," + String(element1));
+      if(JSON.stringify(version0[element0]) == JSON.stringify(version1[element1])){
+        
+        interestElement = element0;
+        array0.push(element0);
+        //console.log(JSON.stringify(interestElement));
+        
+      }
+    }
+    index0 = interestElement;
+  }
+  return array0;
+}
+
+
+function beginningInsertVersion0(version0, version1, array0, array1){
+  if(version0.slice(0, array0[0]).length == 0){
+    if(version1.slice(0, array1[0]).length != 0){
+      for(let ops = 0; ops < version1.slice(0, array1[0]).length; ops++){
+        version0.splice(ops, 0, version1.slice(0, array1[0])[ops]);
+      }
+      //version0.splice(0, 0, version1.slice(0, array1[0]));
+    }
+  }
+  return version0;
+}
+
+function beginningInsertVersion1(version0, version1, array0, array1){
+  if(version1.slice(0, array1[0]).length == 0){
+    if(version0.slice(0, array0[0]).length != 0){
+      for(let ops = 0; ops < version0.slice(0, array1[0]).length; ops++){
+        version1.splice(ops, 0, version0.slice(0, array1[0])[ops]);
+      }
+      //version0.splice(0, 0, version1.slice(0, array1[0]));
+    }
+  }
+  return version0;
+}
+
+function middleInsertVersion0(version0, version1, array0, array1){
+  for(i = 0; i < array0.length - 1; i++){
+    if(version0.slice(array0[i], array0[i + 1]).length == 0){
+      if(version1.slice(array1[i], array1[i + 1]).length != 0){
+        for(let ops = array1[i]; ops < version1.slice(array1[i], array1[i + 1]).length; ops++){
+          version0.splice(ops, 0, version1.slice(array1[i], array1[i + 1])[ops]);
+        }
+        //version0.splice(0, 0, version1.slice(0, array1[0]));
+      }
+    }
+  }
+  return version0;
+}
+
+function middleInsertVersion0(version0, version1, array0, array1){
+  for(i = 0; i < array1.length - 1; i++){
+    if(version1.slice(array1[i], array1[i + 1]).length == 0){
+      if(version0.slice(array0[i], array0[i + 1]).length != 0){
+        for(let ops = array0[i]; ops < version0.slice(array0[i], array0[i + 1]).length; ops++){
+          version1.splice(ops, 0, version0.slice(array0[i], array0[i + 1])[ops]);
+        }
+        //version0.splice(0, 0, version1.slice(0, array1[0]));
+      }
+    }
+  }
+  return version1;
+}
+
+function merging(version0, version1){
+  fs.writeFile('file.json', JSON.stringify(version0), (err) => {
+    if(err) console.log(err);
+    else{
+      git.add('file.json').commit('1st part').push('https://github.com/SrinjoySanyal/BSP2022Quill.git', 'master')
+      .then(fs.writeFile('file.json', JSON.stringify(version1),(err) => {
+        if(err) console.log(err);
+        else{
+          git.pull('https://github.com/SrinjoySanyal/BSP2022Quill.git', 'master', {'--rebase': 'true'}).branch(['<Merging>'])
+          .checkoutBranch('Merging', 'master').add('file.json').commit('2nd part').push('https://github.com/SrinjoySanyal/BSP2022Quill.git')
+          .mergeFromTo('https://github.com/SrinjoySanyal/BSP2022Quill.git', 'master').then((response) => {console.log(response);}).catch((err) => {console.log(err);})
+        }
+      }));
+    }
+  });
+}
+
+function displayDelta(version){
+  finalDelta = []
+  let l = version.length;
+  for(let i = 1; i < l; i++){
+    version.splice(i, 0, {ops: [{insert: '\n'}]});
+  }
+  version.push({ops: [{insert: '\n'}]});
+  //alert(JSON.stringify(version));
+  for(let j = 0; j < version.length; j++){
+    for(let k = 0; k < version[j].ops.length; k++){
+      finalDelta.push(version[j].ops[k]);
+    }
+  }
+
+  return finalDelta;
 }
 
 /**
@@ -33,37 +148,36 @@ function uploadDelta(version1){
       version0 = JSON.parse(data);
 
       /**
-       * Goal: Create an array with the common indices
+       * Goal: Create array1 containing the indices of the elements of version1 in version0 and array0 containing the indices 
+       * of the elements of version0 in version1
        */
-      array1 = commonIndices(version0,version1);
+      array1 = commonIndicesVersion1(version0,version1);
+      array0 = commonIndicesVersion0(version0,version1);
 
-      if(version0.slice(0, array1[0]).length == 0){
-        if(version1.slice(0, array1[0]).length != 0){
-          version0.splice(0, version1.slice(0, array1[0]));
-        }
-      }
+      /*
+      If some ops had been inserted in the beginning of Version1 but not of Version 0,
+      then those ops are added to the beginning of Version0
+      */
+      version0 = beginningInsertVersion0(version0, version1, array0, array1);
 
-      if(version1.slice(0, array1[0]).length == 0){
-        if(version0.slice(0, array1[0]).length != 0){
-          version1.splice(0, version0.slice(0, array1[0]));
-        }
-      }
+      /*
+      If some ops had been inserted in the beginning of Version0 but not of Version1,
+      then those ops are added to the beginning of Version1
+      */
+      version1 = beginningInsertVersion1(version0, version1, array0, array1);
 
-      for(i = 0; i < array1.length - 1; i++){
-        if(version0.slice(array1[i] + 1, array1[i + 1]).length == 0){
-          if(version1.slice(array1[i] + 1, array1[i + 1]).length != 0){
-            version0.splice(i, version1.slice(array1[i] + 1, array1[i + 1]));
-          }
-        }
-      }
+      /*
+      If some ops had been inserted in the middle of Version1 but not of Version0,
+      then those ops are added to the beginning of Version0
+      */
+      version0 = middleInsertVersion0(version0, version1, array0, array1);
 
-      for(j = 0; j < array1.length - 1; j++){
-        if(version1.slice(array1[j] + 1, array1[j + 1]).length == 0){
-          if(version0.slice(array1[j] + 1, array1[j + 1]).length != 0){
-            version1.splice(j, version0.slice(array1[j] + 1, array1[j + 1]).length)
-          }
-        }
-      }
+      /*
+      If some ops had been inserted in the middle of Version0 but not of Version1,
+      then those ops are added to the beginning of Version1
+      */
+      version1 = middleInsertVersion1(version0, version1, array0, array1);
+
 
       fs.writeFile('file.json', JSON.stringify(version0), (err) => {
         if(err){
@@ -92,34 +206,188 @@ function uploadDelta(version1){
   });
 }
 
-console.log('Testing commonIndices');
 
-console.log('   Test 1');
+console.log("Test 1")
+console.log('      Testing commonIndicesVersion1');
+
 const commonIndices_Test1_version0 = [
   {
     ops: [
       {insert: 'Sringjoy'}
     ]
+  },
+  {
+    ops: [
+      {insert: 'yes'}
+    ]
   }
-]
+];
 const commonIndices_Test1_version1 = [
   {
     ops: [
       {insert: 'Tomer'}
     ]
+  },
+  {
+    ops: [
+      {insert: 'Sringjoy'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'yes'}
+    ]
   }
-]
-const commonIndices_Test1_array1 = []
+];
 
-const commonIndices_Test1_result = JSON.stringify(commonIndices(commonIndices_Test1_version0,commonIndices_Test1_version1));
+const commonIndices_Test1_array1 = [1,2];
+
+const commonIndices_Test1_result = JSON.stringify(commonIndicesVersion1(commonIndices_Test1_version0,commonIndices_Test1_version1));
 if (commonIndices_Test1_result === JSON.stringify(commonIndices_Test1_array1))
   console.log("   Test 1 succeeded");
 else {
-  console.log("   Test 1 failed");
+  console.log("   Test 1.1 failed");
   console.log(`   Calling commonIndices on ${JSON.stringify(commonIndices_Test1_version0)} and ${JSON.stringify(commonIndices_Test1_version1)} should result with ${JSON.stringify(commonIndices_Test1_array1)}. Instead, we get ${JSON.stringify(commonIndices_Test1_result)}`);
 }
 
-let test = '[{"ops":[{"insert":"Srinjoy"}]}]';
+console.log('     Testing commonIndicesVersion0');
+const commonIndicesVersion0_Test1_array1 = [0,1];
 
-//if (uploadDelta(JSON.parse(test)) == null)
-//  console.log("Test 1 failed");
+const commonIndicesVersion0_Test1_result = JSON.stringify(commonIndicesVersion0(commonIndices_Test1_version0,commonIndices_Test1_version1));
+if (commonIndicesVersion0_Test1_result === JSON.stringify(commonIndicesVersion0_Test1_array1))
+  console.log("   Test 1.2 succeeded");
+else {
+  console.log("   Test 1.2 failed");
+  console.log(`   Calling commonIndices on ${JSON.stringify(commonIndices_Test1_version0)} and ${JSON.stringify(commonIndices_Test1_version1)} should result with ${JSON.stringify(commonIndicesVersion0_Test1_array1)}. Instead, we get ${JSON.stringify(commonIndicesVersion0_Test1_result)}`);
+}
+
+console.log('    Testing beginningInsertVersion0');
+beginningInsertVersion0_Test1_result = [
+  {
+    ops: [
+      {insert: 'Tomer'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'Sringjoy'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'yes'}
+    ]
+  }
+];
+
+const beginningInsertVersion0_Test1_version0 = beginningInsertVersion0(commonIndices_Test1_version0,commonIndices_Test1_version1,[0,1],[1,2]);
+if (JSON.stringify(beginningInsertVersion0_Test1_result) == JSON.stringify(beginningInsertVersion0_Test1_version0))
+  console.log("   Test 1.3 succeeded");
+else {
+  console.log("   Test 1.3 failed");
+  console.log(`   Calling commonIndices on ${JSON.stringify(commonIndices_Test1_version0)} and ${JSON.stringify(commonIndices_Test1_version1)} should result with ${JSON.stringify(beginningInsertVersion0_Test1_result)}. Instead, we get ${JSON.stringify(beginningInsertVersion0_Test1_version0)}`);
+}
+
+console.log('Test 2');
+console.log('    Testing middleInsertVersion0');
+
+const Test2_version0 = [
+  {
+    ops: [
+      {insert: 'Tomer'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'yes'}
+    ]
+  }
+];
+
+const Test2_version1 = [
+  {
+    ops: [
+      {insert: 'Tomer'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'Sringjoy'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'yes'}
+    ]
+  }
+];
+
+const middleInsertVersion0_test = [
+  {
+    ops: [
+      {insert: 'Tomer'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'Sringjoy'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'yes'}
+    ]
+  }
+];
+
+const middleInsertVersion0_result = middleInsertVersion0(Test2_version0, Test2_version1, commonIndicesVersion0(Test2_version0,Test2_version1), commonIndicesVersion1(Test2_version0,Test2_version1));
+
+if (JSON.stringify(middleInsertVersion0_test) == JSON.stringify(middleInsertVersion0_result))
+  console.log("   Test 2.1 succeeded");
+else {
+  console.log("   Test 2 failed");
+  console.log(`   Calling middleInsertVersion0 on ${JSON.stringify(Test2_version0)} and ${JSON.stringify(Test2_version1)} should result with ${JSON.stringify(middleInsertVersion0_test)}. Instead, we get ${JSON.stringify(middleInsertVersion0_version0)}`);
+}
+
+console.log('    Testing displayDelta');
+
+const displayDelta_test = [
+  {insert: 'Tomer'},
+  {insert: '\n'},
+  {insert: 'yes'},
+  {insert: '\n'}
+];
+const displayDelta_result = displayDelta(Test2_version0);
+if(JSON.stringify(displayDelta_result) == JSON.stringify(displayDelta_test)) console.log('   Test 2.2 succeeded');
+else {
+  console.log("   Test 2.2 failed");
+  console.log(`   Calling displayDelta on ${JSON.stringify(Test2_version0)} should result with ${JSON.stringify(displayDelta_test)}. Instead, we get ${JSON.stringify(displayDelta_result)}`);
+}
+
+const merging1 = [
+  {
+    ops: [
+      {insert: 'Tomer'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'yes'}
+    ]
+  }
+];
+
+const merging2 = [
+  {
+    ops: [
+      {insert: 'Tomer'}
+    ]
+  },
+  {
+    ops: [
+      {insert: 'no'}
+    ]
+  }
+];
+
+merging(merging1, merging1);
